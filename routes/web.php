0b53f71ee5138c6d233878b222cc45062e0245ac
eeetwosix <jayemfonaciersgv@gmail.com>
2019-07-11 08:13:25 +0800

    Added a PostsController for database interaction

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
 Route::get('/projects', 'PagesController@projects');
+
+Route::resource('posts', 'PostsController');
